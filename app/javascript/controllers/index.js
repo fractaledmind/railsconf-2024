fc8f89cbb096cf66bbd99143cca55fc27367feb7
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-20 19:45:36 +0200

    Style the application layout
    Install and configure the [`@tailwindcss/typography`][] and [`@tailwindcss/forms`][] plugins.
    Then, update the `app/views/layouts/application.html.erb` file to provide a fun, monochromatic design.
    
    Since, I am personally using an older version of Safari, also replace the `allow_browser versions: :modern`
    directive in the `app/controllers/application_controller.rb` file with a custom configuration that allows
    Safari 17.0.
    
    Layout
    ---
    
    ![application layout](https://raw.githubusercontent.com/fractaledmind/railsconf-2024/main/.github/images/application-layout.png)
    
    [`@tailwindcss/typography`]: https://github.com/tailwindlabs/tailwindcss-typography
    [`@tailwindcss/forms`]: https://github.com/tailwindlabs/tailwindcss-forms

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import FadeController from "./fade_controller"
+application.register("fade", FadeController)
