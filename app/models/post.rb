8ed2e1231b795def7eea9a91adeb5b9a1906dd7f
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-22 21:47:44 +0200

    Scaffold Comments
    In addition to submitting Posts, Users can also comment on Posts. Like Posts, Comments can be publicly read, but only authenticated Users can create, edit, or delete Comments. Of course, only the User that created the Comment can edit or delete it.
    
    The `Comment` resource was scaffolded and then tweaked. The scaffold command was:
    
    ```
    bin/rails generate scaffold Comment \
      post:belongs_to \
      user:belongs_to \
      body:text
    ```

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, counter_cache: true
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
 end
